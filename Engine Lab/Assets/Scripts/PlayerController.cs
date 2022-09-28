12c9b458ed2d3772277c1919806c4a94219614fc
milenano <mvnovakovic@gmail.com>
2022-09-27 22:03:10 -0400

    Week 1 and 2 Folder, Customized Folder

---
+++ Engine Lab/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public static PlayerController instance;
+
+    //player movement
+    public PlayerAction inputAction;
+    Vector2 move;
+    Vector2 rotate;
+    private float walkSpeed = 5f;
+    // public Camera playerCamera;
+    //Vector3 cameraRotation;
+
+    //player jump
+    Rigidbody rb; //rigid body
+    private float distanceToGround;
+    private bool isGrounded = true;
+    public float jump = 5f;
+
+    //player animation
+    Animator playerAnimator;
+    private bool isWalking = false;
+
+    //shooting
+    public GameObject bullet;
+    public Transform projectilePos;
+
+    private void OnEnable()
+    {
+        inputAction.Player.Enable();
+    }
+    private void OnDisable()
+    {
+        inputAction.Player.Disable();
+    }
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        inputAction = new PlayerAction();
+
+        inputAction.Player.Move.performed += cntxt => move = cntxt.ReadValue<Vector2>();
+        inputAction.Player.Move.canceled += cntxt => move = Vector2.zero;
+
+        inputAction.Player.Jump.performed += cntxt => Jump();
+
+        inputAction.Player.Shoot.performed += cntxt => Shoot();
+
+        rb = GetComponent<Rigidbody>();
+        playerAnimator = GetComponent<Animator>();
+        distanceToGround = GetComponent<Collider>().bounds.extents.y;
+
+    }
+
+    private void Jump()
+    {
+        if (isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jump);
+            isGrounded = false;
+        }
+    }
+
+    public void Shoot()
+    {
+        Rigidbody bulletRb = Instantiate(bullet, projectilePos.position, Quaternion.identity).GetComponent<Rigidbody>();
+        bulletRb.AddForce(transform.forward * 32f, ForceMode.Impulse);
+        bulletRb.AddForce(transform.up * 5f, ForceMode.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * move.y * Time.deltaTime * walkSpeed, Space.Self);
+        transform.Translate(Vector3.right * move.x * Time.deltaTime * walkSpeed, Space.Self);
+
+        isGrounded = Physics.Raycast(transform.position, -Vector3.up, distanceToGround);
+    }
+}
---
+++ Engine Lab/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+
+{
+    public static ScoreManager instance;
+    int score = 0;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if(!instance)
+        {
+            instance = this;
+        }
+    }
+
+    public void ChangeScore(int coinValue)
+    {
+        score += coinValue;
+        Debug.Log(score);
+    }
+}
